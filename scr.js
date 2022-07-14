9b98da8ee431cb680c537426296ba7140c8dcee9
Niraj Kumar Dewangan <niraj.dewangan1608@gmail.com>
2022-07-14 12:18:49 +0530

    initial commit

---
+++ scr.js
@@ -0,0 +1,21 @@
+// let element = document.getElementById("first");
+// // element = element.className;
+// element.style.color = "red";
+// element.innerText = "asu is good boy";
+// element.innerHTML = "<b> asu is good boy </b>";
+// console.log(element);
+
+let sel = document.querySelector("#first");
+sel = document.querySelector("#first");
+sel = document.querySelector(".child");
+sel = document.querySelector("#first");
+sel = document.querySelector("div");
+console.log(sel);
+
+let elm = document.getElementsByClassName("child");
+elm = document.getElementsByClassName("child");
+elm = document.getElementsByTagName("div");
+Array.from(elm).forEach((element) => {
+  console.log(element);
+});
+// console.log(elm);
